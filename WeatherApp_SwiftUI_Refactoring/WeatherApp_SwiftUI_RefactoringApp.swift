1091e8ec3cbe64a5d4ae8176b3522b162a853a24
KSYong <ericyong95@naver.com>
2022-11-10 13:32:47 +0900

    Initial Commit

---
+++ WeatherApp_SwiftUI_Refactoring/WeatherApp_SwiftUI_RefactoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_SwiftUI_RefactoringApp.swift
+//  WeatherApp_SwiftUI_Refactoring
+//
+//  Created by 권승용 on 2022/11/10.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_SwiftUI_RefactoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
